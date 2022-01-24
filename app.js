b99d637c4e677061004388c6e34d712ba342da09
Arnab <arnabde319@gmail.com>
2022-01-24 08:58:03 +0530

    change

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const userRoutes = require('./routes/users');
 
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/camp-zone';
 
-
 async function main() {
   await mongoose.connect(dbUrl);
 }
